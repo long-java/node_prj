9fbed823b12bed21039692e09e51551c003268e1
Long Phạm Tiến <phamtienlong0309@gmail.com>
2026-01-12 18:37:16 +0700

    update content

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<center><h1>Long ❤ Lam</h1></center>");
+  res.send("<center><h1>Render OK</h1></center>");
 });
 
 const port = process.env.PORT || 3000;  

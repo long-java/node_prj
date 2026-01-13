5a8b5a40cac4dac7dbe172622836e56c529d5bab
Long Phạm Tiến <phamtienlong0309@gmail.com>
2026-01-13 21:01:30 +0700

    update 1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<center><h1>Render OK</h1></center>");
+  res.send("<center><h1>Render OK 1</h1></center>");
 });
 
 const port = process.env.PORT || 3000;  
